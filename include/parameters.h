5703b7bb8500849a43a6528554a8885d7b801f53
burakyueksel <mail.burakyuksel@gmail.com>
2023-06-03 07:29:03 +0200

    add drone type to the parameters

--- include/parameters.h
+++ include/parameters.h
@@ -8,10 +8,23 @@
 #include <eigen3/Eigen/Dense> // Include Eigen library for vector and matrix operations
 #include <vector>
 
+struct DroneTypes
+{
+    enum Type
+    {
+        MC_QUAD,
+        MC_HEXA,
+        MC_OCTO,
+        FW_MONO,
+    };
+};
+
 struct DroneParameters
 {
+    DroneTypes::Type droneType;
     double mass;
     Eigen::Matrix3d inertiaMatrix;
+    Eigen::Vector3d cogOffset;
 };
 
 namespace Parameters {
--- src/parameters.cpp
+++ src/parameters.cpp
@@ -9,6 +9,7 @@ namespace Parameters {
 
         // Drone 1 parameters
         DroneParameters drone1Params;
+        drone1Params.droneType = DroneTypes::MC_QUAD;
         drone1Params.mass = 1.0; // Set the mass for drone 1
         drone1Params.inertiaMatrix << 1.0, 0.0, 0.0,
                                       0.0, 2.0, 0.0,
@@ -17,6 +18,7 @@ namespace Parameters {
 
         // Drone 2 parameters
         DroneParameters drone2Params;
+        drone2Params.droneType = DroneTypes::MC_HEXA;
         drone2Params.mass = 1.5; // Set the mass for drone 2
         drone2Params.inertiaMatrix << 4.0, 0.0, 0.0,
                                       0.0, 5.0, 0.0,
@@ -25,6 +27,7 @@ namespace Parameters {
 
         // Drone 3 parameters
         DroneParameters drone3Params;
+        drone3Params.droneType = DroneTypes::MC_OCTO;
         drone3Params.mass = 2.0; // Set the mass for drone 3
         drone3Params.inertiaMatrix << 7.0, 0.0, 0.0,
                                       0.0, 8.0, 0.0,
@@ -33,6 +36,7 @@ namespace Parameters {
 
         // Drone 4 parameters
         DroneParameters drone4Params;
+        drone4Params.droneType = DroneTypes::FW_MONO;
         drone4Params.mass = 2.5; // Set the mass for drone 4
         drone4Params.inertiaMatrix << 10.0, 0.0, 0.0,
                                       0.0, 11.0, 0.0,
