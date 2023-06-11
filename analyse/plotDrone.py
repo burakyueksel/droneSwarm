038ecf9b80987b0eba8f01a1d9946dadb85c6d00
burakyueksel <mail.burakyuksel@gmail.com>
2023-06-11 22:29:46 +0200

    extend static plotting

--- analyse/plotDrone.py
+++ analyse/plotDrone.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 
 # Read positions from the file
@@ -11,15 +12,23 @@ with open("drone_positions.txt", "r") as file:
         positions[drone_id].append([x, y, z])
 
 # Create a 3D plot
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+fig = plt.figure(figsize=(8, 6))
+ax = fig.add_subplot(221, projection='3d')
+
+# Plot every x-th point
+every_xth_data = 100
 
 # Plot the drone positions by ID
 for drone_id, drone_positions in positions.items():
-    x = [position[0] for position in drone_positions]
-    y = [position[1] for position in drone_positions]
-    z = [-position[2] for position in drone_positions] # z comes in NED. Revert sign for negative down, positive up
-    ax.scatter(x, y, z, marker='o', label=f"Drone {int(drone_id)}")
+    x = [position[0] for position in drone_positions[::every_xth_data]]
+    y = [position[1] for position in drone_positions[::every_xth_data]]
+    z = [-position[2] for position in drone_positions[::every_xth_data]] # z comes in NED. Revert sign for negative down, positive up
+
+    ax.scatter(x, y, z, marker='.', label=f"Drone {int(drone_id)}", linewidths=0.1)
+
+    # Plot the start and end positions as a black cross
+    ax.scatter(x[0], y[0], z[0], marker='x', color='black')
+    ax.scatter(x[-1], y[-1], z[-1], marker='x', color='black')
 
 # Set plot labels and title
 ax.set_xlabel('X')
@@ -27,6 +36,52 @@ ax.set_ylabel('Y')
 ax.set_zlabel('Z')
 ax.set_title('Drone Positions')
 ax.legend()
+
+
+# XY plot
+ax_xy = fig.add_subplot(222)
+for drone_id, drone_positions in positions.items():
+    x = [position[0] for position in drone_positions[::every_xth_data]]
+    y = [position[1] for position in drone_positions[::every_xth_data]]
+    ax_xy.plot(x, y, '.', label=f"Drone {int(drone_id)}")
+    # Plot the start and end positions as a black cross
+    ax_xy.plot(x[0], y[0], marker='x', color='black')
+    ax_xy.plot(x[-1], y[-1], marker='x', color='black')
+ax_xy.set_xlabel('X')
+ax_xy.set_ylabel('Y')
+ax_xy.set_title('XY Plane')
+ax_xy.legend()
+
+# XZ plot
+ax_xz = fig.add_subplot(223)
+for drone_id, drone_positions in positions.items():
+    x = [position[0] for position in drone_positions[::every_xth_data]]
+    z = [-position[2] for position in drone_positions[::every_xth_data]]  # z comes in NED. Revert sign for negative down, positive up
+    ax_xz.plot(x, z, '.', label=f"Drone {int(drone_id)}")
+    # Plot the start and end positions as a black cross
+    ax_xz.plot(x[0], z[0], marker='x', color='black')
+    ax_xz.plot(x[-1], z[-1], marker='x', color='black')
+ax_xz.set_xlabel('X')
+ax_xz.set_ylabel('Z')
+ax_xz.set_title('XZ Plane')
+ax_xz.legend()
+
+# YZ plot
+ax_yz = fig.add_subplot(224)
+for drone_id, drone_positions in positions.items():
+    y = [position[1] for position in drone_positions[::every_xth_data]]
+    z = [-position[2] for position in drone_positions[::every_xth_data]]  # z comes in NED. Revert sign for negative down, positive up
+    ax_yz.plot(y, z, '.', label=f"Drone {int(drone_id)}")
+    # Plot the start and end positions as a black cross
+    ax_yz.plot(y[0], z[0], marker='x', color='black')
+    ax_yz.plot(y[-1], z[-1], marker='x', color='black')
+ax_yz.set_xlabel('Y')
+ax_yz.set_ylabel('Z')
+ax_yz.set_title('YZ Plane')
+ax_yz.legend()
+
+# Adjust the spacing between subplots
+plt.tight_layout()
 
 # Show the plot
 plt.show()
